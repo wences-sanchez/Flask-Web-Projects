145cdf72cd7d4a72d301331abbfa15835d5c8721
wences-sanchez <wenceslaosanchezpino@gmail.com>
2020-08-07 16:24:16 +0200

    Improved app before using more advanced features

--- british_greeting/first_app.py
+++ british_greeting/first_app.py
@@ -4,7 +4,8 @@ first_app = Flask(__name__)
 
 @first_app.route('/')
 def index():
-	return 'Good morning, Sir Wences'
+	return 'Good morning, Sir Wences. Welcomme to '
+		'your cool new view! Click the link below'
 
 if __name__ == '__main__':
 	first_app.run()
